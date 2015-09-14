dfa1b19c592ab1362e7d1c9907a38f887bb35d34
John Häggerud <john.haggerud@lnu.se>
2015-09-14 12:03:42 +0200

    Creates the assignment

---
+++ src/part-1/make-html/make-html.js
@@ -0,0 +1,33 @@
+/**
+ * Namespace for Make Html assignment
+ *
+ * @namespace MakeHtml
+ * @author thajo
+ * @version 1.0.0
+ */
+var MakeHtml = (function() {
+    "use strict";
+
+
+    return {
+
+        makeStartTag: function(element) {
+            // TODO: Write this code first
+        },
+
+        makeEndTag: function(element) {
+            // TODO: Write this code second
+        },
+
+        makeElement: function(element, text) {
+
+            // TODO: Write this code third
+        },
+
+        makeSnippet: function(blocks) {
+            // TODO: Write this code last
+        }
+    };
+}());
+
+module.exports = MakeHtml;
