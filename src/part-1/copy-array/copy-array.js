4b88d364d35709b219861935e1dfe21082788355
John Häggerud <john.haggerud@lnu.se>
2015-09-14 12:45:47 +0200

    Create the assignment

---
+++ src/part-1/copy-array/copy-array.js
@@ -0,0 +1,23 @@
+/**
+ * Module for copy array
+ *
+ * @author thajo
+ * @version 1.0.0
+ */
+
+"use strict";
+
+/**
+ * The function takes an Array, makes a copy if it and adds the number parameter at the end
+ * The original array should be untouched
+ *
+ * @param {Array.<Number>} data - An array with numbers
+ * @param {Number} number - A number to add at the end
+ * @throws {TypeError} First argument must be an array, second argument must be a Number.
+ * @throws {Error} First argument can not be an empty array.
+ * @returns {Array}
+ */
+exports.addToCopy = function(data, number) {
+
+    // TODO: Your code here
+};
