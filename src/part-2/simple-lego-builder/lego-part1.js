7fc4434f29bef404f5973714aa84543a4f0f5c9a
Leitet <johan.leitet@lnu.se>
2015-09-29 12:02:15 +0200

    Initial

---
+++ src/part-2/simple-lego-builder/lego-part1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Lego builder part1
+ *
+ * @author Johan Leitet
+ * @version 1.0.0
+ */
+
+/**
+ * Returns a new object
+ *
+ * @param {Number} x Number horisontal knobs on the lego brick
+ * @param {Number} y Number vertical knobs on the lego brick
+ * @param {Number} z Hight of the lego brick
+ * @param {String} color Color of the lego brick ("blue", "red", "green" etc.)
+ * @returns {{x: Number, y: Number, z: Number, color: String, toString: function, range: function}}
+ */
+exports.createLego = function(x, y, z, color) {
+    // TODO: Write your code here. The functions should return an object representing a lego brick
+};
---
+++ src/part-2/simple-lego-builder/lego-part3.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Creates a new Lego object
+ *
+ * @constructor
+ * @param {Number} x Number horisontal knobs on the lego brick
+ * @param {Number} y Number vertical knobs on the lego brick
+ * @param {Number} z Hight of the lego brick
+ * @param {String} color Color of the lego brick ("blue", "red", "green" etc.)
+ */
+function Lego(x, y, z, color) {
+    // TODO: Write all code in this constructor function
+
+}
+
+// TODO: Write Prototype functions here
+
+exports.Lego = Lego;
