2c2d860c3830d36872cbcff3dc6288d81e6badd6
Leitet <johan.leitet@lnu.se>
2015-09-29 12:55:06 +0200

    render not range

--- src/part-2/simple-lego-builder/lego-part1.js
+++ src/part-2/simple-lego-builder/lego-part1.js
@@ -10,11 +10,11 @@
 /**
  * Returns a new object
  *
- * @param {Number} x Number horisontal knobs on the lego brick
- * @param {Number} y Number vertical knobs on the lego brick
- * @param {Number} z Hight of the lego brick
- * @param {String} color Color of the lego brick ("blue", "red", "green" etc.)
- * @returns {{x: Number, y: Number, z: Number, color: String, toString: function, range: function}}
+ * @param {Number} x Number horisontal knobs on the lego brick default 2
+ * @param {Number} y Number vertical knobs on the lego brick default 4
+ * @param {Number} z Hight of the lego brick default 2
+ * @param {String} color Color of the lego brick ("blue", "red", "green" etc.) default red
+ * @returns {{x: Number, y: Number, z: Number, color: String, toString: function, render: function}}
  */
 exports.createLego = function(x, y, z, color) {
     // TODO: Write your code here. The functions should return an object representing a lego brick
