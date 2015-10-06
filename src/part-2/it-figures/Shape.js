bdffedbecf5fb873d960d4f23b21a67cf793df2b
Mats Loock <mats.loock@lnu.se>
2015-10-06 13:19:29 +0200

    Initial commit

---
+++ src/part-2/it-figures/Shape.js
@@ -0,0 +1,15 @@
+/**
+ * Module for shapes.
+ *
+ * @author TODO: Write your name here!
+ * @version 1.0.0
+ */
+
+"use strict";
+
+// TODO: Write your code here!
+
+// Exports
+module.exports = Shape;
+module.exports.Ellipse = Ellipse;
+module.exports.Rectangle = Rectangle;
