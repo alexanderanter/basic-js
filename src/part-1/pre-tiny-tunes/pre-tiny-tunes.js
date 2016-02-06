4793bebf5a4f3115124e1ba7e7ccd8942599858a
Alexander Anter <alexanderanter@gmail.com>
2016-02-06 23:09:31 +0100

    Making all functions work

--- src/part-1/pre-tiny-tunes/pre-tiny-tunes.js
+++ src/part-1/pre-tiny-tunes/pre-tiny-tunes.js
@@ -36,7 +36,7 @@ exports.roundUp = function(number) {
     // The variable "number" is a number. This number should be rounded up. E.g. 15.2 should return 16.
     // You could try to stop the debugger at the return-line to investigate the number.
     // Your task is to populate roundedNumber with the rounded number.
-
+    roundedNumber = Math.ceil(number);
     return roundedNumber;
 };
 
@@ -52,7 +52,7 @@ exports.roundDown = function(number) {
     // Your code here - change the code under to make the test go green
     // The variable "number" is a number. This number should be rounded down. E.g. 15.9 should return 15.
     // Your task is to populate roundedNumber with the rounded number.
-
+    roundedNumber = Math.floor(number);
     return roundedNumber;
 };
 
@@ -68,7 +68,7 @@ exports.round = function(number) {
     // Your code here - change the code under to make the test go green
     // The variable "number" is a number. This number should be rounded. E.g. 15.9 should return 16. and 15.4 should return 15.
     // Your task is to populate roundedNumber with the rounded number.
-
+    roundedNumber = Math.round(number);
     return roundedNumber;
 };
 
@@ -81,7 +81,7 @@ exports.getStringLength = function(str) {
 
     // The variable "str" is a string. Your task is to return the length of the string. E.g. "Det blauser hä i Kamar" should return the number 22
 
-    return;
+    return str.length;
 };
 
 /**
@@ -90,7 +90,7 @@ exports.getStringLength = function(str) {
  * @returns {String} The first letter in the String str
  */
 exports.getFirstLetter = function(str) {
-
+    return str.charAt(0);
 };
 
 /**
@@ -99,7 +99,7 @@ exports.getFirstLetter = function(str) {
  * @returns {Number} The position of the question mark in the string.
  */
 exports.getPositionOfQuestionMark = function(str) {
-
+    return str.indexOf("?");
 };
 
 /**
@@ -109,7 +109,11 @@ exports.getPositionOfQuestionMark = function(str) {
  * @returns {Boolean} true if str1 equals str2 and false if they are not equal
  */
 exports.isEqual = function(str1, str2) {
-
+    if (str1 === str2) {
+        return true;
+    }   else {
+        return false;
+    }
 };
 
 /**
@@ -118,7 +122,11 @@ exports.isEqual = function(str1, str2) {
  * @returns {Boolean} true if number is odd, false if it is even.
  */
 exports.isOdd = function(number) {
-
+    if (number % 2 === 0) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 /**
@@ -127,7 +135,11 @@ exports.isOdd = function(number) {
  * @returns {Boolean} true if number is even, false if it is odd.
  */
 exports.isEven = function(number) {
-
+    if (number % 2 === 0) {
+        return true;
+    }else {
+        return false;
+    }
 }
 
 /*
