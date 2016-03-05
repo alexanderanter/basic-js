6bd772e5cd03e66e7e869a1980d6af4d8f8eb765
Alexander Anter <alexanderanter@gmail.com>
2016-03-05 14:16:58 +0100

    cleaned up still not working

--- src/part-1/parse-querystring/parse-querystring.js
+++ src/part-1/parse-querystring/parse-querystring.js
@@ -21,13 +21,12 @@
 exports.parse = function(url) {
   url = "http://lnu.se?key1=value1&key2=value2&key3=value3";
   var urlLength = url.length;
-  var countOfValues = 0;
   var ampPos = url.indexOf('&');
-  var ampPositions = [];
+
   var urlList = {};
   //check if there is a value at all
   if (url.indexOf('?') !== -1 ){
-    countOfValues = 1;
+
     //push key1 and value1
     urlList[url.split("?")[1].split("=")[0]] = url.split("=")[1].split("&")[0];
   }
@@ -35,40 +34,11 @@ exports.parse = function(url) {
 
     var urlCutOff = url.substring(ampPos, urlLength);
     urlList[urlCutOff.split("&")[1].split("=")[0]] = urlCutOff.split("=")[1].split("&")[0];
-  console.log(urlCutOff);
-
-//obsolete code
-    ampPositions.push(ampPos);
-    countOfValues++;
-
 
     ampPos = url.indexOf('&', ampPos + 1);
 
   }
-  console.log(countOfValues);
-  for(var i = 0; i < countOfValues;i++){
-    console.log("tjoho");
-  }
-
-
-  // for( amount of & ) {
-  //     put them in array and sort their charAt values
-  //
-  //     cut the string out between the char values
-  //     then use the same url split code to check whats between & and =
-  //     then check whats between = and & for the value
-  //
-
-  // }
-
-  // console.log(url.split("?")[1].split("=")[0]); //key1
-  // console.log(url.split("=")[1].split("&")[0]); //value1
-// var test1 = url.split("?")[1].split("=")[0];
-// var test2 = url.split("=")[1].split("&")[0];
-
-
-
-  console.log(urlList);
+    console.log(urlList);
 
     return urlList;
 };
