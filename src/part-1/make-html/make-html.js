7663b1623ae9ecad8a314fe7df992b5af11b0170
Alexander Anter <alexanderanter@gmail.com>
2016-03-05 19:16:01 +0100

    makestart makeend and makeelement functions working

--- src/part-1/make-html/make-html.js
+++ src/part-1/make-html/make-html.js
@@ -13,19 +13,35 @@ var MakeHtml = (function() {
 
         makeStartTag: function(element) {
             // TODO: Write this code first
+            return "<" + element + ">";
+
         },
 
         makeEndTag: function(element) {
             // TODO: Write this code second
+              return "</" + element + ">";
         },
 
         makeElement: function(element, text) {
 
             // TODO: Write this code third
+            var completeElement;
+            completeElement = MakeHtml.makeStartTag(element) + text + MakeHtml.makeEndTag(element);
+            return completeElement;
         },
 
         makeSnippet: function(blocks) {
             // TODO: Write this code last
+
+            var keys = Object.keys(blocks); // ['make','color','year']
+            var snippet;
+            keys.forEach(function(key) {
+
+              var testVal = (blocks[key]);
+              console.log(testVal);
+            });
+
+
         }
     };
 }());
