62b247ff4dc9a3c58113ab335edd271c5e56593a
Alexander Anter <alexanderanter@gmail.com>
2016-03-08 00:43:02 +0100

    Sub sequence challenge is now working as expected

--- src/part-1/sub-sequence/sub-sequence.js
+++ src/part-1/sub-sequence/sub-sequence.js
@@ -19,83 +19,30 @@ exports.isSequence = function(arr, sub) {
 
     var validResult = false;
     var slicedPiece;
-    var found = false;
-    firstArray.forEach(function(key){
-        if(key === secondArray[0]) {
 
-            slicedPiece = firstArray.slice(firstArray.indexOf(key),secondArray.length);
-            console.log(slicedPiece);
+    var startingPoint = 0;
+    var alreadyFoundAt;
+    firstArray.forEach(function(key) {
+        if (key === secondArray[0]) {
 
-            if(slicedPiece.length === secondArray.length){
-              for(var i = 0; i < secondArray.length; i++){
-                if(slicedPiece[i] === secondArray[i] && found !== true){
+            if (!alreadyFoundAt) {
+              startingPoint = firstArray.indexOf(key);
+            } else {
+              startingPoint = firstArray.indexOf(key, (alreadyFoundAt +1));
+            }
+
+            alreadyFoundAt = startingPoint;
+            slicedPiece = firstArray.slice( startingPoint,(startingPoint + secondArray.length));
+            if (slicedPiece.length === secondArray.length) {
+              for (var i = 0; i < secondArray.length; i++) {
+                if (slicedPiece[i] === secondArray[i]) {
                   validResult = true;
-                }else {
+                } else {
                   validResult = false;
-                
                 }
               }
             }
-            // count = key;
-            // var i = 0;
-            // while (i < secondArray.length){
-            //     lastVal = secondArray[i];
-            //     i++;
-            // }
-
         }
     });
-
-
-
-    //
-    //
-    // var count;
-    // var found = false;
-    // var lastVal;
-    // var nextVal = 0;
-    // firstArray.forEach(function(key){
-    //     if(key === secondArray[0]) {
-    //         count = key;
-    //         var i = 0;
-    //         while (i < secondArray.length){
-    //             lastVal = secondArray[i];
-    //             if (lastVal === secondArray[i] && !found){
-    //                 found = true;
-    //                 validResult = true;
-    //             }else if (lastVal !== secondArray[i]) {
-    //                 if (count === secondArray[i]) {
-    //                     validResult = true;
-    //                 }else {
-    //                     validResult = false;
-    //                 }
-    //
-    //                 console.log(lastVal + "lastVal")
-    //                 console.log(secondArray[i] + "nextval");
-    //
-    //                 i++;
-    //                 found = false;
-    //                 count++;
-    //             }
-
-/*
-                lastVal = i;
-                console.log("this is last val" + lastVal + "this is new val" + secondArray[i]);
-*/
-
-/*                nextVal = i + 1;*/
-/*                console.log(secondArray[nextVal] + "wtf");
-                if(secondArray[nextVal] !== count){
-                    count++;
-                }*/
-
-
-    //             i++;
-    //         }
-    //
-    //     }
-    // });
-
     return validResult;
-
 };
