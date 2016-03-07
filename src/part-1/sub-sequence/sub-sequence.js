a25de073143c001b5db398d1343d572c0b0925b3
AANTER <Alexander.Anter@gtech.com>
2016-03-07 16:29:04 +0100

    unfinished version of sub sequence challenge

--- src/part-1/sub-sequence/sub-sequence.js
+++ src/part-1/sub-sequence/sub-sequence.js
@@ -14,6 +14,56 @@
  * @returns {boolean}
  */
 exports.isSequence = function(arr, sub) {
+    var firstArray = arr.slice();
+    var secondArray = sub.slice();
+    var validResult = false;
+    var count;
+    var found = false;
+    var lastVal;
+    var nextVal = 0;
+    firstArray.forEach(function(key){
+        if(key === secondArray[0]) {
+            count = key;
+            var i = 0;
+            while (i < secondArray.length){
+                lastVal = secondArray[i];
+                if (lastVal === secondArray[i] && !found){
+                    found = true;
+                    validResult = true;
+                }else if (lastVal !== secondArray[i]) {
+                    if (count === secondArray[i]) {
+                        validResult = true;
+                    }else {
+                        validResult = false;
+                    }
+
+                    console.log(lastVal + "lastVal")
+                    console.log(secondArray[i] + "nextva");
+
+
+                    i++;
+                    found = false;
+                    count++;
+                }
+
+/*
+                lastVal = i;
+                console.log("this is last val" + lastVal + "this is new val" + secondArray[i]);
+*/
+
+/*                nextVal = i + 1;*/
+/*                console.log(secondArray[nextVal] + "wtf");
+                if(secondArray[nextVal] !== count){
+                    count++;
+                }*/
+
+
+                i++;
+            }
+
+        }
+    });
+
+    return validResult;
 
 };
-
