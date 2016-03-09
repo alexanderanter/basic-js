0c4fd93fa76dc92f82e759e1d5061d7969bd9173
alexanderanter <alexanderanter@gmail.com>
2016-03-09 17:22:22 +0100

    start building the object ellipse

--- src/part-2/epic-ellipse/Ellipse.js
+++ src/part-2/epic-ellipse/Ellipse.js
@@ -8,6 +8,19 @@
 "use strict";
 
 // TODO: Write your code here!
+function Ellipse(a, b) {
+        this.a = undefined;
+        this.b = undefined;
+}
 
+Ellipse.prototype.area = function() {
+    this.var = thing";
+};
+Ellipse.prototype.circumference = function() {
+    this.var = thing";
+};
+Ellipse.prototype.toString = function() {
+    this.var = "thing";
+};
 // Exports
 module.exports = Ellipse;
