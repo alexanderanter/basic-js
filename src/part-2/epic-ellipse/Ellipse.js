aeabde037a60611a7b080d0ae9ff6d12bff14a82
Alexander Anter <alexanderanter@gmail.com>
2016-03-09 23:45:22 +0100

    Ellipse now working and passing all tests challenge completed

--- src/part-2/epic-ellipse/Ellipse.js
+++ src/part-2/epic-ellipse/Ellipse.js
@@ -1,26 +1,24 @@
 /**
  * Module for the type Ellipse.
  *
- * @author TODO: Write your name here!
+ * @author : alexanderanter
  * @version 1.0.0
  */
 
 "use strict";
 
-// TODO: Write your code here!
 function Ellipse(a, b) {
-        this.a = undefined;
-        this.b = undefined;
+        this.a = a;
+        this.b = b;
 }
 
 Ellipse.prototype.area = function() {
-    this.var = thing";
+    return Math.PI * this.a * this.b;
 };
 Ellipse.prototype.circumference = function() {
-    this.var = thing";
+    return Math.PI * Math.sqrt(2 * this.a * this.a + 2 * this.b * this.b);
 };
 Ellipse.prototype.toString = function() {
-    this.var = "thing";
-};
-// Exports
+  return "{ a: " + this.a + ", b: " + this.b + ", area: " + this.area().toFixed(1) + ", circumference: " + this.circumference().toFixed(1) + " }";
+};// Exports
 module.exports = Ellipse;
