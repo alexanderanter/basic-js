022e78cd05fcb8f3e978adc0f6d3fcc0ebad38ae
Alexander Anter <alexanderanter@gmail.com>
2016-03-11 23:18:45 +0100

    building the code body for the league challenge

--- src/part-2/the-leauge/leauge.js
+++ src/part-2/the-leauge/leauge.js
@@ -7,7 +7,39 @@
  */
 
 
-// TODO - your code here
+function Leauge() {
+  this.teams = [];
+}
 
+Leauge.prototype.addTeam = function() {
+  var result;
+
+  return result;
+};
+Leauge.prototype.playMatches = function() {
+  var result;
+
+  return result;
+};
+Leauge.prototype.registerResult = function() {
+  var result;
+
+  return result;
+};
+Leauge.prototype.playMatch = function() {
+  var result;
+
+  return result;
+};
+Leauge.prototype.getTableString = function() {
+  var result;
+
+  return result;
+};
+Leauge.prototype.addTeam = function() {
+  var result;
+
+  return result;
+};
 // exports the object for others tho use
 module.exports = Leauge;
--- src/part-2/the-leauge/team.js
+++ src/part-2/the-leauge/team.js
@@ -5,7 +5,28 @@
  * @author: alexanderanter
  * @version 1.0.0
  */
-// TODO - your code here
+
+function Team(teamName) {
+   if(teamName) {
+     this.name = teamName;
+     this.points = 0;
+   } else {
+     throw new Error("NOthing sent!");
+   }
+
+}
+
+Team.prototype.toTableRow = function(padding) {
+  var result;
+  this.padding = padding || 25;
+  result = this.name;
+  for(var i = 4; i < this.padding; i++) {
+      result += " ";
+  }
+  result += this.points;
+
+  return result;
+};
 
 // exports the object
 module.exports = Team;
