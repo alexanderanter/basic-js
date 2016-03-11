97a6fbc28d6b4d22c5440fdc66a044301405f36a
alexanderanter <alexanderanter@gmail.com>
2016-03-11 16:14:26 +0100

    adding a code body to the it-figures challenge

--- src/part-2/it-figures/Shape.js
+++ src/part-2/it-figures/Shape.js
@@ -7,7 +7,44 @@
 
 "use strict";
 
-// TODO: Write your code here!
+function Shape(length, height) {
+    this.length = length;
+    this.height = height;
+}
+
+Shape.prototype.toString = function() {
+    console.log(this.length + this.height);
+};
+
+function Ellipse(length, height) {
+    Shape.call(this, length, height); // this = Ellipse, by using call we can control that this will be this this
+}
+
+Ellipse.prototype = Object.create(Shape.prototype);
+Ellipse.prototype.constructor = Ellipse;
+
+Ellipse.prototype.area = function() {
+
+};
+
+Ellipse.prototype.circumference = function() {
+
+};
+
+function Rectangle(length, height) {
+    Shape.call(this, length, height); // this = Ellipse, by using call we can control that this will be this this
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+
+};
+
+Rectangle.prototype.circumference = function() {
+
+};
 
 // Exports
 module.exports = Shape;
